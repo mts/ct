35de04acca5fa4e5e2a5f9519a9c4b39090478d1
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-05 17:13:41 +0100

    implement button

--- packages/client/src/pattern/atom/action/Button/Button.jsx
+++ packages/client/src/pattern/atom/action/Button/Button.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import cx from 'classnames'
+import { button, buttonOuterBorder, buttonFirstInnerBorder, buttonSecondInnerBorder, buttonContainer } from './Button.scss'
 import { buttonDefaultProps, buttonPropTypes } from './Button.props'
 
 export function Button({ className }) {
-  return <div className={className}>Button under construction</div>
+  return (
+    <a href="/" className={cx(className, button)}>
+      <div className={buttonOuterBorder}>
+        <div className={buttonFirstInnerBorder}>
+          <div className={buttonSecondInnerBorder}>
+            <div className={buttonContainer}>CHECKOUT</div>
+          </div>
+        </div>
+      </div>
+    </a>
+  )
 }
 
 Button.defaultProps = buttonDefaultProps
