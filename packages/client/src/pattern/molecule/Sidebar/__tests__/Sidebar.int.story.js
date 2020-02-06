9f29c9f94c34ec2814d7ac027b48bf07f79efb0c
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-06 11:25:43 +0100

    add sidebar story

--- packages/client/src/pattern/molecule/Sidebar/__tests__/Sidebar.int.story.js
+++ packages/client/src/pattern/molecule/Sidebar/__tests__/Sidebar.int.story.js
@@ -4,4 +4,4 @@ import { getInfo } from '../../../../../../../.storybook/library'
 
 const defaultRenderInfo = getInfo('default')
 
-storiesOf('Patttern/Atom/Layout/Sidebar', module).add(defaultRenderInfo.text, () => defaultRender, defaultRenderInfo.parameters)
+storiesOf('Patttern/Molecule/Layout/Sidebar', module).add(defaultRenderInfo.text, () => defaultRender, defaultRenderInfo.parameters)
