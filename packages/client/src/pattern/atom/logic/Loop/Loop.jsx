156e2a3874bf238dd1f82cafc1916e95511662af
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-06 16:11:49 +0100

    implement Loop

--- packages/client/src/pattern/atom/logic/Loop/Loop.jsx
+++ packages/client/src/pattern/atom/logic/Loop/Loop.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
-import cx from 'classnames'
-import { loop } from './Loop.scss'
+import uuidv1 from 'uuid/v1'
 import { loopDefaultProps, loopPropTypes } from './Loop.props'
+import { getRange } from '../../../../../../library/src/array'
 
-export function Loop({ className }) {
-  return <div className={cx(className, loop)}>Loop under construction</div>
+export function Loop({ className, count }) {
+  function StringTimesCount() {
+    const fooString = 'Foo'
+    const barString = 'Bar'
+    const fooBarString = `${fooString}${barString}`
+
+    const parsedCount = parseInt(count, 10)
+
+    if (Number.isNaN(parsedCount) || parsedCount === -1) {
+      return null
+    }
+
+    const rangedItems = getRange({ range: count, indexSeed: 1, valueSeed: 0 })
+
+    return rangedItems.map(rangedItem => {
+      let string = rangedItem
+
+      if (rangedItem % 3 === 0) {
+        string = fooString
+      }
+
+      if (rangedItem % 5 === 0) {
+        string = barString
+      }
+
+      if (rangedItem % 3 === 0 && rangedItem % 5 === 0) {
+        string = fooBarString
+      }
+
+      return <div key={uuidv1()}>{string}</div>
+    })
+  }
+
+  return (
+    <div className={className}>
+      <StringTimesCount />
+    </div>
+  )
 }
 
 Loop.defaultProps = loopDefaultProps
