dd0b17d486ae5d56baa8ee84a188da78f2b57a2a
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-07 12:09:18 +0100

    restore case 4 story name

--- packages/client/src/pattern/atom/logic/Loop/__tests__/Loop.int.story.js
+++ packages/client/src/pattern/atom/logic/Loop/__tests__/Loop.int.story.js
@@ -4,4 +4,4 @@ import { getInfo } from '../../../../../../../../.storybook/library'
 
 const defaultRenderInfo = getInfo('default')
 
-storiesOf('Case 4 Print', module).add(defaultRenderInfo.text, () => defaultRender, defaultRenderInfo.parameters)
+storiesOf('Case 4 Loop', module).add(defaultRenderInfo.text, () => defaultRender, defaultRenderInfo.parameters)
